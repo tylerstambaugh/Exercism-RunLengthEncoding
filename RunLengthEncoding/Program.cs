d8354d0610cbeebca526048512d78965f008bfa2
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-11-02 21:01:55 -0400

    Add project files.

---
+++ RunLengthEncoding/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+Console.WriteLine(RunLengthEncoding.Encode("AABBBCCCC"));
+Console.ReadLine();
+
+public static class RunLengthEncoding
+{
+    public static string Encode(string input)
+    {
+        string encodedString = "";
+        int numChars = 1;
+        int index = 0;
+        int inputLength = input.Length;
+
+        while(index <= input.Length)
+        {
+            char c = input[index];
+            if (index < inputLength)
+            {
+                if (c == input[index + 1])
+                {
+                    numChars++;
+                    index++;
+                }
+
+                else if (c != input[index + 1] && numChars > 1)
+                {
+                    encodedString += numChars + c.ToString();
+                    numChars = 1;
+                    index++;
+                }
+            }
+
+            else if (index == input.Length)
+            {
+                encodedString += c.ToString();
+                index++;
+                numChars = 1;
+            }
+        }
+
+        return encodedString;
+    }
+
+    public static string Decode(string input)
+    {
+        string decodedString = input;
+        return decodedString;
+    }
+}
