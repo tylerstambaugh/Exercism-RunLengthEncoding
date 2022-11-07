7db5a7e68f1dd770f90a294d8913d1fe1cf1c328
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-11-07 18:07:34 -0500

    2 tests failing

--- RunLengthEncoding/Program.cs
+++ RunLengthEncoding/Program.cs
@@ -1,7 +1,11 @@
 ﻿
 
-Console.WriteLine(RunLengthEncoding.Encode("AABBBCCCC"));
-Console.WriteLine(RunLengthEncoding.Encode("XYZ"));
+//Console.WriteLine(RunLengthEncoding.Encode("AABBBCCCC"));
+//Console.WriteLine(RunLengthEncoding.Encode("XYZ"));
+
+// Console.WriteLine(RunLengthEncoding.Decode("2A3B4C"));
+Console.WriteLine(RunLengthEncoding.Decode("XYZ"));
+
 Console.ReadLine();
 
 public static class RunLengthEncoding
@@ -61,7 +65,46 @@ public static class RunLengthEncoding
 
     public static string Decode(string input)
     {
-        string decodedString = input;
+        string decodedString = "";
+        string inputTrimmed = input.Trim();
+        bool isDigit;
+        
+        if (input == "")
+        {
+            decodedString = "";
+        }
+
+        for (int i = 0; i < inputTrimmed.Length; i++)
+        {
+            char c = input[i];
+            int countOfDigits = 1;
+            int inputLength = inputTrimmed.Length;
+            if (Char.IsDigit(c))
+            {
+                countOfDigits = Int16.Parse(c.ToString());
+            }
+
+            if (!Char.IsDigit(c) && countOfDigits == 1)
+            {
+                decodedString += c;
+            }
+
+            if (i < inputLength - 1)
+            {
+                if (!Char.IsDigit(input[i + 1]) && countOfDigits > 1)
+                {
+                    string decodedChars = "";
+                    for (int j = countOfDigits; j > 0; j--)
+                    {
+                        decodedChars += input[i + 1].ToString();
+                    }
+                    i++;
+                    decodedString += decodedChars;
+                }
+            }
+
+        }
+
         return decodedString;
     }
 }
